1b0c1ce0a23624dd92e5e420e013999fa8ae2597
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-06-04 21:12:48 -0700

    Serial test sketch for BT SPP

---
+++ BluetoothSerialTest.ino
@@ -0,0 +1,20 @@
+
+void setup() {
+  Serial1.begin(9600);
+  while (!Serial1) {}
+}
+
+void loop() {
+  ping();
+  if (Serial1.available()) {
+    Serial1.write(Serial1.read());
+  }
+}
+
+void ping() {
+  static long lasttime;
+  if ((lasttime + 60000) < millis()) {
+    Serial1.write("Ping!");
+    lasttime = millis();
+  }
+}
